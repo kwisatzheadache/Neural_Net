76971ede02c23ba847636c48d06a61fe5893e3e3
kwisatzheadache <michaelhorvath87@gmail.com>
2017-03-11 22:45:32 -0500

    Update sense.ex

--- lib/sense.ex
+++ lib/sense.ex
@@ -1,4 +1,6 @@
-
+#This isn't doing anything productive yet. The idea is to automate the signal process.
+#Need to generate a keymap for the neuron/iex_process/actuator pid's so they can 
+#be fed to each component so they can all send/receive.
 defmodule Sense do
   def input(input, neuron, map) do
     send neuron, {map, input}
